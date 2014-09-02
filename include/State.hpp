f6ebf0abe2853d92ce4fe536f7b831e249c4dc98
bloesch <bloeschm@ethz.ch>
2014-09-02 17:25:08 +0200

    started with predictionModel

--- include/State.hpp
+++ include/State.hpp
@@ -18,7 +18,10 @@ namespace LWF{
 template<unsigned int S, unsigned int V, unsigned int Q>
 class StateSVQ{
  public:
-  static const unsigned int D_ = S+(V+Q)*3;
+  static const unsigned int S_ = S;
+  static const unsigned int V_ = V;
+  static const unsigned int Q_ = Q;
+  static const unsigned int D_ = S_+(V_+Q_)*3;
   typedef Eigen::Matrix<double,D_,1> DiffVec;
   typedef Eigen::Matrix<double,D_,D_> CovMat;
   StateSVQ(){
@@ -26,92 +29,92 @@ class StateSVQ{
     setIdentity();
   };
   double t_;
-  double scalarList[S];
-  Eigen::Vector3d vectorList[V];
-  rot::RotationQuaternionPD quaternionList[Q];
-  void boxPlus(const DiffVec& vecIn, StateSVQ<S,V,Q>& stateOut) const{
+  double scalarList[S_];
+  Eigen::Vector3d vectorList[V_];
+  rot::RotationQuaternionPD quaternionList[Q_];
+  void boxPlus(const DiffVec& vecIn, StateSVQ<S_,V_,Q_>& stateOut) const{
     unsigned int index = 0;
-    for(unsigned int i=0;i<S;i++){
+    for(unsigned int i=0;i<S_;i++){
       stateOut.scalarList[i] = scalarList[i]+vecIn(index);
       index += 1;
     }
-    for(unsigned int i=0;i<V;i++){
+    for(unsigned int i=0;i<V_;i++){
       stateOut.vectorList[i] = vectorList[i]+vecIn.block(index,0,3,1);
       index += 3;
     }
-    for(unsigned int i=0;i<Q;i++){
+    for(unsigned int i=0;i<Q_;i++){
       stateOut.quaternionList[i] = quaternionList[i].boxPlus(vecIn.block(index,0,3,1));
       index += 3;
     }
     stateOut.t_ = t_;
   }
-  void boxMinus(const StateSVQ<S,V,Q>& stateIn, DiffVec& vecOut) const{
+  void boxMinus(const StateSVQ<S_,V_,Q_>& stateIn, DiffVec& vecOut) const{
     unsigned int index = 0;
-    for(unsigned int i=0;i<S;i++){
+    for(unsigned int i=0;i<S_;i++){
       vecOut(index) = scalarList[i]-stateIn.scalarList[i];
       index += 1;
     }
-    for(unsigned int i=0;i<V;i++){
+    for(unsigned int i=0;i<V_;i++){
       vecOut.block(index,0,3,1) = vectorList[i]-stateIn.vectorList[i];
       index += 3;
     }
-    for(unsigned int i=0;i<Q;i++){
+    for(unsigned int i=0;i<Q_;i++){
       vecOut.block(index,0,3,1) = quaternionList[i].boxMinus(stateIn.quaternionList[i]);
       index += 3;
     }
   }
   void print() const{
     std::cout << "Scalars:" << std::endl;
-    for(unsigned int i=0;i<S;i++){
+    for(unsigned int i=0;i<S_;i++){
       std::cout << s(i) << std::endl;
     }
     std::cout << "Vectors:" << std::endl;
-    for(unsigned int i=0;i<V;i++){
+    for(unsigned int i=0;i<V_;i++){
       std::cout << v(i).transpose() << std::endl;
     }
     std::cout << "Quaternions:" << std::endl;
-    for(unsigned int i=0;i<Q;i++){
+    for(unsigned int i=0;i<Q_;i++){
       std::cout << q(i) << std::endl;
     }
   }
   void setIdentity(){
-    for(unsigned int i=0;i<S;i++){
+    for(unsigned int i=0;i<S_;i++){
       scalarList[i] = 0.0;
     }
-    for(unsigned int i=0;i<V;i++){
+    for(unsigned int i=0;i<V_;i++){
       vectorList[i].setZero();
     }
-    for(unsigned int i=0;i<Q;i++){
+    for(unsigned int i=0;i<Q_;i++){
       quaternionList[i].setIdentity();
     }
   }
   void fix(){
-    for(unsigned int i=0;i<Q;i++){
+    for(unsigned int i=0;i<Q_;i++){
       q(i).fix();
     }
   }
   const double& s(unsigned int i) const{
-    assert(i<S);
+    assert(i<S_);
     return scalarList[i];
   };
   double& s(unsigned int i) {
-    assert(i<S);
+    assert(i<S_);
     return scalarList[i];
   };
   const Eigen::Matrix<double,3,1>& v(unsigned int i) const{
-    assert(i<V);
+    assert(i<V_);
     return vectorList[i];
   };
   Eigen::Matrix<double,3,1>& v(unsigned int i) {
-    assert(i<V);
+    assert(i<V_);
     return vectorList[i];
   };
   const rot::RotationQuaternionPD& q(unsigned int i) const{
-    assert(i<Q);
+    assert(i<Q_);
     return quaternionList[i];
   };
   rot::RotationQuaternionPD& q(unsigned int i) {
-    assert(i<Q);
+    assert(i<Q_);
     return quaternionList[i];
   };
 };
--- src/testPredictionModel.cpp
+++ src/testPredictionModel.cpp
@@ -1,106 +1,73 @@
+#include "PredictionModel.hpp"
 #include "State.hpp"
 #include "gtest/gtest.h"
 #include <assert.h>
 
-// The fixture for testing class VectorState
-class VectorStateTest : public ::testing::Test {
- protected:
-  VectorStateTest() {
-    testVector1_ << 2.1, -0.2, -1.9, 0.2;
-    testVector2_ << -10.6, 0.2, 25, -105.2;
-  }
-  virtual ~VectorStateTest() {
-  }
-  static const unsigned int D_ = 4;
-  LWF::VectorState<D_> testVectorState1_;
-  LWF::VectorState<D_> testVectorState2_;
-  LWF::VectorState<D_>::DiffVec difVec_;
-  Eigen::Matrix<double,D_,1> testVector1_;
-  Eigen::Matrix<double,D_,1> testVector2_;
+class State: public LWF::StateSVQ<4,3,2>{
+ public:
+  State(){};
+  ~State(){};
+};
+class PredictionMeas: public LWF::StateSVQ<2,2,2>{
+ public:
+  PredictionMeas(){};
+  ~PredictionMeas(){};
+};
+class PredictionModelExample: public LWF::PredictionModel<State,PredictionMeas,10>{
+ public:
+  PredictionModelExample(){};
+  ~PredictionModelExample(){};
 };
 
-// Test constructors
-TEST_F(VectorStateTest, constructors) {
-  LWF::VectorState<D_> testVectorState1;
-  for(int i=0;i<D_;i++){
-    ASSERT_EQ(testVectorState1[i],0.0);
-  }
-}
-
-// Test setIdentity
-TEST_F(VectorStateTest, setIdentity) {
-  testVectorState1_.setIdentity();
-  for(int i=0;i<D_;i++){
-    ASSERT_EQ(testVectorState1_[i],0.0);
-  }
-}
-
-// Test plus and minus
-TEST_F(VectorStateTest, plusAndMinus) {
-  testVectorState1_.vector_ = testVector1_;
-  difVec_ = testVector2_;
-  testVectorState1_.boxPlus(difVec_,testVectorState2_);
-  for(int i=0;i<D_;i++){
-    ASSERT_EQ(testVectorState2_[i],testVector1_[i]+testVector2_[i]);
-  }
-  testVectorState2_.boxMinus(testVectorState1_,difVec_);
-  for(int i=0;i<D_;i++){
-    ASSERT_NEAR(difVec_[i],testVector2_[i],1e-6);
-  }
-}
-
-// The fixture for testing class StateSVQ
-class StateSVQTest : public ::testing::Test {
+// The fixture for testing class PredictionModel
+class PredictionModelTest : public ::testing::Test {
  protected:
-  StateSVQTest() {
-    assert(V_>=Q_-1);
+  PredictionModelTest() {
+    assert(State::V_>=State::Q_-1);
     testScalar1_[0] = 4.5;
     testScalar2_[0] = -17.34;
-    for(int i=1;i<S_;i++){
+    for(int i=1;i<State::S_;i++){
       testScalar1_[i] = testScalar1_[i-1] + i*i*46.2;
       testScalar2_[i] = testScalar2_[i-1] - i*i*0.01;
     }
     testVector1_[0] << 2.1, -0.2, -1.9;
     testVector2_[0] << -10.6, 0.2, -105.2;
-    for(int i=1;i<V_;i++){
+    for(int i=1;i<State::V_;i++){
       testVector1_[i] = testVector1_[i-1] + Eigen::Vector3d(0.3,10.9,2.3);
       testVector2_[i] = testVector2_[i-1] + Eigen::Vector3d(-1.5,12,1785.23);
     }
     testQuat1_[0] = rot::RotationQuaternionPD(4.0/sqrt(30.0),3.0/sqrt(30.0),1.0/sqrt(30.0),2.0/sqrt(30.0));
     testQuat2_[0] = rot::RotationQuaternionPD(0.0,0.36,0.48,0.8);
-    for(int i=1;i<Q_;i++){
+    for(int i=1;i<State::Q_;i++){
       testQuat1_[i] = testQuat1_[i-1].boxPlus(testVector1_[i-1]);
       testQuat2_[i] = testQuat2_[i-1].boxPlus(testVector2_[i-1]);
     }
   }
-  virtual ~StateSVQTest() {
+  virtual ~PredictionModelTest() {
   }
-  static const unsigned int S_ = 4;
-  static const unsigned int V_ = 3;
-  static const unsigned int Q_ = 2;
-  LWF::StateSVQ<S_,V_,Q_> testState1_;
-  LWF::StateSVQ<S_,V_,Q_> testState2_;
-  LWF::StateSVQ<S_,V_,Q_>::DiffVec difVec_;
-  double testScalar1_[S_];
-  double testScalar2_[S_];
-  Eigen::Vector3d testVector1_[V_];
-  Eigen::Vector3d testVector2_[V_];
-  rot::RotationQuaternionPD testQuat1_[Q_];
-  rot::RotationQuaternionPD testQuat2_[Q_];
+  State testState1_;
+  State testState2_;
+  State::DiffVec difVec_;
+  double testScalar1_[State::S_];
+  double testScalar2_[State::S_];
+  Eigen::Vector3d testVector1_[State::V_];
+  Eigen::Vector3d testVector2_[State::V_];
+  rot::RotationQuaternionPD testQuat1_[State::Q_];
+  rot::RotationQuaternionPD testQuat2_[State::Q_];
 };
 
 // Test constructors
-TEST_F(StateSVQTest, constructors) {
-  LWF::StateSVQ<S_,V_,Q_> testState1;
-  for(int i=0;i<S_;i++){
+TEST_F(PredictionModelTest, constructors) {
+  State testState1;
+  for(int i=0;i<State::S_;i++){
     ASSERT_EQ(testState1.scalarList[i],0.0);
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     ASSERT_EQ(testState1.vectorList[i](0),0.0);
     ASSERT_EQ(testState1.vectorList[i](1),0.0);
     ASSERT_EQ(testState1.vectorList[i](2),0.0);
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     ASSERT_EQ(testState1.quaternionList[i].w(),1.0);
     ASSERT_EQ(testState1.quaternionList[i].x(),0.0);
     ASSERT_EQ(testState1.quaternionList[i].y(),0.0);
@@ -109,17 +76,17 @@ TEST_F(StateSVQTest, constructors) {
 }
 
 // Test setIdentity
-TEST_F(StateSVQTest, setIdentity) {
+TEST_F(PredictionModelTest, setIdentity) {
   testState1_.setIdentity();
-  for(int i=0;i<S_;i++){
+  for(int i=0;i<State::S_;i++){
     ASSERT_EQ(testState1_.scalarList[i],0.0);
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     ASSERT_EQ(testState1_.vectorList[i](0),0.0);
     ASSERT_EQ(testState1_.vectorList[i](1),0.0);
     ASSERT_EQ(testState1_.vectorList[i](2),0.0);
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     ASSERT_EQ(testState1_.quaternionList[i].w(),1.0);
     ASSERT_EQ(testState1_.quaternionList[i].x(),0.0);
     ASSERT_EQ(testState1_.quaternionList[i].y(),0.0);
@@ -128,32 +95,32 @@ TEST_F(StateSVQTest, setIdentity) {
 }
 
 // Test plus and minus
-TEST_F(StateSVQTest, plusAndMinus) {
-  for(int i=0;i<S_;i++){
+TEST_F(PredictionModelTest, plusAndMinus) {
+  for(int i=0;i<State::S_;i++){
     testState1_.scalarList[i] = testScalar1_[i];
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     testState1_.vectorList[i] = testVector1_[i];
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     testState1_.quaternionList[i] = testQuat1_[i];
   }
-  for(int i=0;i<S_;i++){
+  for(int i=0;i<State::S_;i++){
     testState2_.scalarList[i] = testScalar2_[i];
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     testState2_.vectorList[i] = testVector2_[i];
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     testState2_.quaternionList[i] = testQuat2_[i];
   }
   testState2_.boxMinus(testState1_,difVec_);
   unsigned int index=0;
-  for(int i=0;i<S_;i++){
+  for(int i=0;i<State::S_;i++){
     ASSERT_EQ(difVec_(index),testScalar2_[i]-testScalar1_[i]);
     index ++;
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     ASSERT_EQ(difVec_(index),testVector2_[i](0)-testVector1_[i](0));
     index ++;
     ASSERT_EQ(difVec_(index),testVector2_[i](1)-testVector1_[i](1));
@@ -161,7 +128,7 @@ TEST_F(StateSVQTest, plusAndMinus) {
     ASSERT_EQ(difVec_(index),testVector2_[i](2)-testVector1_[i](2));
     index ++;
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     ASSERT_EQ(difVec_(index),testQuat2_[i].boxMinus(testQuat1_[i])(0));
     index ++;
     ASSERT_EQ(difVec_(index),testQuat2_[i].boxMinus(testQuat1_[i])(1));
@@ -170,39 +137,39 @@ TEST_F(StateSVQTest, plusAndMinus) {
     index ++;
   }
   testState1_.boxPlus(difVec_,testState2_);
-  for(int i=0;i<S_;i++){
+  for(int i=0;i<State::S_;i++){
     ASSERT_NEAR(testState2_.scalarList[i],testScalar2_[i],1e-6);
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     ASSERT_NEAR(testState2_.vectorList[i](0),testVector2_[i](0),1e-6);
     ASSERT_NEAR(testState2_.vectorList[i](1),testVector2_[i](1),1e-6);
     ASSERT_NEAR(testState2_.vectorList[i](2),testVector2_[i](2),1e-6);
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     ASSERT_TRUE(testState2_.quaternionList[i].isNear(testQuat2_[i],1e-6));
   }
 }
 
 // Test accessors
-TEST_F(StateSVQTest, accessors) {
-  for(int i=0;i<S_;i++){
+TEST_F(PredictionModelTest, accessors) {
+  for(int i=0;i<State::S_;i++){
     testState1_.scalarList[i] = testScalar1_[i];
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     testState1_.vectorList[i] = testVector1_[i];
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     testState1_.quaternionList[i] = testQuat1_[i];
   }
-  for(int i=0;i<S_;i++){
+  for(int i=0;i<State::S_;i++){
     testState1_.s(i) = testScalar1_[i];
   }
-  for(int i=0;i<V_;i++){
+  for(int i=0;i<State::V_;i++){
     testState1_.v(i)(0) = testVector1_[i](0);
     testState1_.v(i)(1) = testVector1_[i](1);
     testState1_.v(i)(2) = testVector1_[i](2);
   }
-  for(int i=0;i<Q_;i++){
+  for(int i=0;i<State::Q_;i++){
     ASSERT_TRUE(testState1_.q(i).isNear(testQuat1_[i],1e-6));
   }
 }
