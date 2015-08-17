f4234d230ec24adf29bbe7f98ca857b5f5f6afeb
Michael Neunert <neunertm@student.ethz.ch>
2015-08-18 01:16:14 +0200

    * fixed integer devisions in common.hpp * inlined functions in common.hpp

--- include/lightweight_filtering/common.hpp
+++ include/lightweight_filtering/common.hpp
@@ -21,7 +21,7 @@ typedef rot::RotationQuaternionPD QPD;
 typedef rot::RotationMatrixPD MPD;
 typedef Eigen::Vector3d V3D;
 typedef Eigen::Matrix3d M3D;
-static M3D gSM(const V3D& vec){
+inline M3D gSM(const V3D& vec){
   return kindr::linear_algebra::getSkewMatrixFromVector(vec);
 }
 
@@ -61,25 +61,18 @@ static void enforceSymmetry(LWFMatrix<nRow,nCol,true>& mat){
   mat = 0.5*(mat+mat.transpose()).eval();
 }
 
-static M3D Lmat (V3D a) {
+inline M3D Lmat (const V3D& a) {
   double aNorm = a.norm();
-  double factor1 = 0;
-  double factor2 = 0;
-  M3D ak;
-  M3D ak2;
-  M3D G_k;
-
+  double factor1 = 1.0/2.0;
+  double factor2 = 1.0/6.0;
   // Get sqew matrices
-  ak = kindr::linear_algebra::getSkewMatrixFromVector(a);
-  ak2 = ak*ak;
+  M3D ak(kindr::linear_algebra::getSkewMatrixFromVector(a));
+  M3D ak2(ak*ak);
 
   // Compute factors
   if(aNorm >= 1e-10){
-    factor1 = (1 - cos(aNorm))/pow(aNorm,2);
+    factor1 = (1.0 - cos(aNorm))/pow(aNorm,2);
     factor2 = (aNorm-sin(aNorm))/pow(aNorm,3);
-  } else {
-    factor1 = 1/2;
-    factor2 = 1/6;
   }
 
   return M3D::Identity()-factor1*ak+factor2*ak2;
