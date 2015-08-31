d040db66153ccddf0be9ba3ae9a93f771cbd7d9d
Michael Neunert <neunertm@student.ethz.ch>
2015-09-01 00:15:35 +0200

    more verbose info added clear to MeasurementTimeline to allow proper reset

--- include/lightweight_filtering/FilterBase.hpp
+++ include/lightweight_filtering/FilterBase.hpp
@@ -33,6 +33,10 @@ class MeasurementTimeline{
   void addMeas(const mtMeas& meas, const double& t){
     measMap_[t] = meas;
   }
+  void clear()
+  {
+	  measMap_.clear();
+  }
   void clean(double t){
     while(measMap_.size() > 1 && measMap_.begin()->first<=t){
       measMap_.erase(measMap_.begin());
@@ -129,13 +133,13 @@ class FilterBase: public PropertyHandler{
     std::get<i>(mUpdates_).outlierDetection_.registerToPropertyHandler(&std::get<i>(mUpdates_),"MahalanobisTh");
   }
   void addPredictionMeas(const typename Prediction::mtMeas& meas, double t){
-    if(t<= safeWarningTime_) std::cout << "Warning: included measurements before safeTime" << std::endl;
+    if(t<= safeWarningTime_) std::cout << "Warning: included prediction measurements before safeTime" << std::endl;
     if(t<= frontWarningTime_) gotFrontWarning_ = true;
     predictionTimeline_.addMeas(meas,t);
   }
   template<unsigned int i=0, typename std::enable_if<(i<nUpdates_)>::type* = nullptr>
   void addUpdateMeas(const typename std::tuple_element<i,decltype(mUpdates_)>::type::mtMeas& meas, double t){
-    if(t<= safeWarningTime_) std::cout << "Warning: included measurements before safeTime" << std::endl;
+    if(t<= safeWarningTime_) std::cout << "Warning: included update measurements before safeTime" << std::endl;
     if(t<= frontWarningTime_) gotFrontWarning_ = true;
     std::get<i>(updateTimelineTuple_).addMeas(meas,t);
   }
