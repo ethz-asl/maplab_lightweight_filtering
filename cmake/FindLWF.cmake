68331297192e10955ec1cf270476700201605e5c
Thomas Schneider <schneith@ethz.ch>
2017-08-15 13:48:47 +0200

    Fix cmake discovery of Eigen.

---
+++ cmake/FindLWF.cmake
@@ -0,0 +1,9 @@
+# This file was automatically generated during the installation of the lightweight_filtering library
+# and can be used through cmake to find the corresponding header files. A copy of this
+# file was created in /opt/cmake/share/cmake-3.7/Modules (depending on the CMAKE_ROOT variable).
+
+set(LWF_INCLUDE_DIRS
+/home/user/code/maplab_ws/install/include/LWF/include
+)
+set(LWF_FOUND TRUE)
+message("-- LWF found (include: /home/user/code/maplab_ws/install/include/LWF/include)")
