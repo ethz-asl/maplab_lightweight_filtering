5f97b30db52bd0fd461e3f5feec70072cebe05ec
Marius Fehr <marius.b.fehr@gmail.com>
2018-11-08 13:09:04 +0100

    make outlier check more verbose

--- include/lightweight_filtering/OutlierDetection.hpp
+++ include/lightweight_filtering/OutlierDetection.hpp
@@ -44,7 +44,7 @@ class OutlierDetectionBase{
     outlier_ = d_ > mahalanobisTh_;
     if(outlier_){
       outlierCount_++;
-      VLOG(4) << "Detected outlier - mahalanobis (d / threshold):"  << d_
+      VLOG(2) << "Detected outlier - mahalanobis (d / threshold):"  << d_
               << " > " << mahalanobisTh_ << " - count : " << outlierCount_
               << ".";
     } else {
